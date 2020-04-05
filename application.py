bbbd1c5acc6ee12e1cb30b4cd05d8380a19f6f18
Benjamin Bolte <bkbolte18@gmail.com>
2020-04-04 19:14:47 -0500

    test

--- application.py
+++ application.py
@@ -3,13 +3,13 @@
 from flask import Flask, render_template, url_for
 
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route("/")
+@application.route("/")
 def render_static():
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
